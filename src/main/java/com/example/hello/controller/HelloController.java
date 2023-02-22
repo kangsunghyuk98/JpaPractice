1102b5ee45b33972fcad5ae7772e1c31c90a3585
kangsunghyuk98 <kangsh9820@naver.com>
2023-02-22 14:34:48 +0900

    practice

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,18 @@ public class HelloController {
     public String viewHello(Model model) {
         model.addAttribute("data","hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString (@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello "+name;
     }
 }
